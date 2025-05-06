e3d0611eeb2809a8501cdc59797850da177b1bbe
pix-macmini4 <pix@pixmacmi4deMini.lan>
2025-05-06 15:13:41 +0800

    update install script

--- install_user.sh
+++ install_user.sh
@@ -5,18 +5,34 @@ COLOR_YELLOW='\033[0;33m'
 COLOR_NC='\033[0m'
 
 function brew_install() {
-  if brew info "$1" &>/dev/null; then
+  if brew list | grep "$1" &>/dev/null; then
     echo -e "${COLOR_GREEN}$1 is installed${COLOR_NC}"
   else
     echo -e "${COLOR_YELLOW}$1 is not install${COLOR_NC}"
     brew install "$1"
   fi
 }
+function brew_ui_install() {
+  if brew list | grep "$1" &>/dev/null; then
+    echo -e "${COLOR_GREEN}$1 is installed${COLOR_NC}"
+  else
+    echo -e "${COLOR_YELLOW}$1 is not install${COLOR_NC}"
+    brew install --cask "$1"
+  fi
+}
 
+brew update
+brew upgrade
 brew_install gnu-sed
 brew_install ffmpeg
-brew_install rcm
 brew_install htop
 brew_install btop
 brew_install duf
 brew_install fastfetch
+brew_ui_install visual-studio-code
+brew_install ripgrep
+brew_install the_silver_searcher
+brew_install pyenv
+pyenv install 3.13.0
+pyenv global 3.13.0
+brew_install rcm
--- pack_install_function.sh
+++ pack_install_function.sh
@@ -1,8 +1,16 @@
 function brew_install() {
-  if brew info "$1" &>/dev/null; then
+  if brew list | grep "$1" &>/dev/null; then
     echo -e "${COLOR_GREEN}$1 is installed${COLOR_NC}"
   else
     echo -e "${COLOR_YELLOW}$1 is not install${COLOR_NC}"
     brew install "$1"
+  fi
+}
+function brew_ui_install() {
+  if brew list | grep "$1" &>/dev/null; then
+    echo -e "${COLOR_GREEN}$1 is installed${COLOR_NC}"
+  else
+    echo -e "${COLOR_YELLOW}$1 is not install${COLOR_NC}"
+    brew install --cask "$1"
   fi
 }
